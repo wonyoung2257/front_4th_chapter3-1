9a7d2a9e37a24fbb01bb43fddd492a6a9ff7d403
WonYoung <wy.heo@teamsparta.co>
2025-02-02 17:55:30 +0900

    msw mocking api 추가

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -2,15 +2,56 @@ import { http, HttpResponse } from 'msw';
 
 import { Event } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
+// 데이터를 전역으로 관리면 CUD에 대한 변화를 감지 할 수 있음
+// 단 걸리는 것은 테스트에서 전역으로 관리하는 데이터가 독립적인 테스트 결과에 영향을 주지 않을까 싶음
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const newEvent = (await request.json()) as Event;
+    newEvent.id = crypto.randomUUID();
 
-  http.put('/api/events/:id', async () => {}),
+    return HttpResponse.json(newEvent, {
+      status: 201,
+    });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.put('/api/events/:id', async ({ request, params }) => {
+    const { id } = params;
+    const eventIndex = events.findIndex((event) => event.id === id);
+
+    if (eventIndex > -1) {
+      const newEvent = (await request.json()) as Event;
+      const newEvents = [...events];
+      newEvents[eventIndex] = { ...events[eventIndex], ...newEvent };
+
+      return HttpResponse.json(newEvents[eventIndex], {
+        status: 204,
+      });
+    }
+
+    return HttpResponse.json('Event not found', {
+      status: 404,
+    });
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+    const newEvents = events.filter((event) => event.id !== id);
+
+    if (newEvents) {
+      return HttpResponse.json(null, {
+        status: 204,
+      });
+    }
+
+    return HttpResponse.json(null, {
+      status: 404,
+    });
+  }),
 ];
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+async function prepare() {
+  const { setupWorker } = await import('msw/browser');
+  const { handlers } = await import('./__mocks__/handlers.ts');
+  const worker = setupWorker(...handlers);
+  return worker.start();
+}
+
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+});
